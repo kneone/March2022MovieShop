9c16788ef55bb2713c619203a745fa512c796191
Abhilash <abhilash.gaddam@antra.com>
2022-04-14 12:11:53 -0400

    Added methods fro salt and hash

--- Infrastructure/Services/AccountService.cs
+++ Infrastructure/Services/AccountService.cs
@@ -6,6 +6,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using System.Security.Cryptography;
 
 namespace Infrastructure.Services
 {
@@ -29,9 +31,11 @@ namespace Infrastructure.Services
                 // 
                 throw new Exception("Duplicate Email, please login");
             }
-
-
             // if email does not exists then hash the password with salt and save the salt and hashed password in the database
+            // never ever create your own hashing algoritm
+            // always use inditsry tried and tested algoritmns
+
+
 
             throw new NotImplementedException();
         }
@@ -42,6 +46,28 @@ namespace Infrastructure.Services
             // check if user exists in the database, only if user exisst then compare the hashes 
             // if hashes are equal then correct password.
             throw new NotImplementedException();
+        }
+
+        private string GetRandomSalt()
+        {
+            var randomBytes = new byte[128 / 8];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(randomBytes);
+            }
+
+            return Convert.ToBase64String(randomBytes);
+        }
+
+        private string GetHashedPassword(string password, string salt)
+        {
+            var hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+           password,
+           Convert.FromBase64String(salt),
+           KeyDerivationPrf.HMACSHA512,
+           10000,
+           256 / 8));
+            return hashed;
         }
     }
 }
