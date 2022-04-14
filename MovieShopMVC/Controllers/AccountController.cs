dcfa3c4952404d0f84f89fcf7f48bb975230d0b4
Abhilash <abhilash.gaddam@antra.com>
2022-04-14 11:50:08 -0400

    Added Models and Views for Login and Register

---
+++ MovieShopMVC/Controllers/AccountController.cs
@@ -0,0 +1,41 @@
+﻿using ApplicationCore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MovieShopMVC.Controllers
+{
+    public class AccountController : Controller
+    {
+        
+        [HttpGet]
+        public async Task<IActionResult> Login()
+        {
+            // return the empty view
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterModel model)
+        {
+            // call the account service, user repository -> User Table
+
+            // never save the password as plain text
+
+            // 
+
+
+            return View();
+        }
+    }
+}
